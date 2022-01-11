443bdaa55518d48e0e80dc3b84c0efd77f4aba2d
Elizabeth Rangel Monsalve <elizarangelm24@gmail.com>
2022-01-11 13:31:06 -0500

    Validaciones, organizacion y marcar como hecho

--- js/app.js
+++ js/app.js
@@ -1,55 +1,128 @@
-/**
- * Created by Thomas on 5/28/2015.
- */
 var app = angular.module('groceryListApp', ["ngRoute"]);
 
 app.config(function($routeProvider){
     $routeProvider
         .when("/",{
             templateUrl: "views/groceryList.html",
-            controller: "GroceryListItemsController"
+            controller: "HomeController"
         })
         .when("/addItem",{
             templateUrl: "views/addItem.html",
-            controller: "GroceryListItemsController"
+            controller: "GroceryListItemController"
         })
-        .when("/addItem/:id/",{
-            templateUrl: "views/addItem.html",
-            controller: "GroceryListItemsController"
+        //editar
+        .when('/addItem/edit/:id',{
+            templateUrl: 'views/addItem.html',
+            controller: 'GroceryListItemController'
         })
         .otherwise({
             redirectTo: "/"
         })
 });
-//setup the items in a service
+
 app.service("GroceryService", function(){
 
     var groceryService = {};
-
+    //date es un objeto
     groceryService.groceryItems = [
-        {completed: true, itemName: 'milk', date: '2014-10-00'},
-        {completed: true, itemName: 'cookies', date: '2014-10-01'},
-        {completed: true, itemName: 'ice cream', date: '2014-10-02'},
-        {completed: true, itemName: 'potatoes', date: '2014-10-02'},
-        {completed: true, itemName: 'cereal', date: '2014-10-03'},
-        {completed: true, itemName: 'bread', date: '2014-10-03'},
-        {completed: true, itemName: 'eggs', date: '2014-10-04'},
-        {completed: true, itemName: 'tortillas', date: '2014-10-04'}
+        {id: 1, completed: true, itemName: 'milk', date: new Date("October 1, 2014 11:13:00")},
+        {id: 2, completed: true, itemName: 'cookies', date: new Date("October 1, 2014 11:13:00")},
+        {id: 3, completed: true, itemName: 'ice cream', date: new Date("October 1, 2014 11:13:00")},
+        {id: 4, completed: true, itemName: 'potatoes', date: new Date("October 2, 2014 11:13:00")},
+        {id: 5, completed: true, itemName: 'cereal', date: new Date("October 3, 2014 11:13:00")},
+        {id: 6, completed: true, itemName: 'bread', date: new Date("October 3, 2014 11:13:00")},
+        {id: 7, completed: true, itemName: 'eggs', date: new Date("October 4, 2014 11:13:00")},
+        {id: 8, completed: true, itemName: 'tortillas', date: new Date("October 5, 2014 11:13:00")}
     ];
+
+    //se revisa cada entrada para editar el seleccionado
+    groceryService.findById = function(id){
+        for(var item in groceryService.groceryItems){
+            if(groceryService.groceryItems[item].id === id) {
+                console.log(groceryService.groceryItems[item]);
+                return groceryService.groceryItems[item];
+            }
+        }
+    };
+    //aumentar los id automaticamente, si se tiene
+    groceryService.getNewId = function(){
+
+        if(groceryService.newId){
+            groceryService.newId++;
+            return groceryService.newId;
+        } else {
+            //underscope libreria
+            //que coja el ultimo id(el mas alto) y le sume 1 para que no se repita
+            var maxId = _.max(groceryService.groceryItems, function(entry){ return entry.id;})
+            groceryService.newId = maxId.id + 1;
+            return groceryService.newId;
+        }
+    };
+    //check uncheked
+    groceryService.markCompleted = function(entry){
+        entry.completed = !entry.completed;
+    };
+    //se cra en el controlador y en el servicio
+    groceryService.removeItem = function(entry){
+        var index = groceryService.groceryItems.indexOf(entry);
+
+        groceryService.groceryItems.splice(index, 1);
+    };
+
+    groceryService.save = function(entry) {
+
+        var updatedItem = groceryService.findById(entry.id);
+
+        if(updatedItem){
+
+            updatedItem.completed = entry.completed;
+            updatedItem.itemName = entry.itemName;
+            updatedItem.date = entry.date;
+
+        }else {
+            entry.id = groceryService.getNewId();
+            groceryService.groceryItems.push(entry);
+        }
+
+    };
 
     return groceryService;
 
 });
 
 app.controller("HomeController", ["$scope", "GroceryService", function($scope, GroceryService) {
-    $scope.appTitle = GroceryService.groceryItems[0].itemName;
-
-
-}]);
-//agregar servicio al controaldor
-app.controller("GroceryListItemsController", ["$scope", "$routeParams", "GroceryService", function($scope, $routeParams, GroceryService){
 
     $scope.groceryItems = GroceryService.groceryItems;
+    //eliminar
+    $scope.removeItem = function(entry){
+        GroceryService.removeItem(entry);
+    };
+    //si el producto se compro
+    $scope.markCompleted = function(entry){
+        GroceryService.markCompleted(entry);
+    };
 
-    $scope.rp = "Route Paramater Value: " + $routeParams.id;
 }]);
+
+app.controller("GroceryListItemController", ["$scope", "$routeParams", "$location", "GroceryService", function($scope, $routeParams, $location, GroceryService){
+    //si no se tiene el id se crea una nueva, si si, se reemplaza/para actualizar
+    if(!$routeParams.id) {
+        $scope.groceryItem = {id: 0, completed: false, itemName: "", date: new Date()};
+    }else{
+        $scope.groceryItem = _.clone(GroceryService.findById(parseInt($routeParams.id)));
+    }
+
+
+    $scope.save = function(){
+        GroceryService.save( $scope.groceryItem );
+        $location.path("/");
+    };
+
+}]);
+//se crea una directive para mostrar el elemento con una vista
+app.directive("tbGroceryItem", function(){
+    return{
+        restrict: "E",
+        templateUrl: "views/groceryItem.html"
+    }
+});
